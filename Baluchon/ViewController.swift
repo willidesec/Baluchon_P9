d6bfbb64103a672289ea6e30db56eeb4972fb6c4
William <william.desecot@hotmail.fr>
2018-09-25 18:22:46 +0200

    Initial Commit

---
+++ Baluchon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Baluchon
+//
+//  Created by William on 25/09/2018.
+//  Copyright © 2018 William Désécot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
