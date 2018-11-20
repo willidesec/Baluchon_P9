5115de68adcae2d1bbdbc5d674d0b12424e9b312
William <william.desecot@hotmail.fr>
2018-11-20 12:42:48 +0100

    Add Bonus.PDF

--- Baluchon/Controller/WeatherController/WeatherCell.swift
+++ Baluchon/Controller/WeatherController/WeatherCell.swift
@@ -26,24 +26,24 @@ class WeatherCell: UITableViewCell {
     // MARK: - PROPERTIES
     var weather: WeatherChannel! {
         didSet {
-            cityLabel.text = weather.location.city
-            cityLabel.adjustsFontSizeToFitWidth = true
-            temperatureLabel.text = "\(weather.item.condition.temp)°C"
-            weatherInformationLabel.text = weather.item.condition.text
-            weatherInformationLabel.adjustsFontSizeToFitWidth = true
+//            cityLabel.text = weather.location.city
+//            cityLabel.adjustsFontSizeToFitWidth = true
+//            temperatureLabel.text = "\(weather.item.condition.temp)°C"
+//            weatherInformationLabel.text = weather.item.condition.text
+//            weatherInformationLabel.adjustsFontSizeToFitWidth = true
         }
     }
     
     // MARK: - METHODS
     private func addShadow() {
-        whiteView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.7).cgColor
-        whiteView.layer.shadowRadius = 4.0
-        whiteView.layer.shadowOffset = CGSize(width: 0, height: 2.0)
-        whiteView.layer.shadowOpacity = 0.4
+//        whiteView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.7).cgColor
+//        whiteView.layer.shadowRadius = 4.0
+//        whiteView.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+//        whiteView.layer.shadowOpacity = 0.4
     }
     
     private func addCornerRadius() {
-        whiteView.layer.cornerRadius = 10
+//        whiteView.layer.cornerRadius = 10
     }
     
 }
