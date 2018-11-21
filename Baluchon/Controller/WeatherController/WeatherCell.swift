84665be0cfd8d3a75463ce99f4b664f33cc67b3f
William <william.desecot@hotmail.fr>
2018-11-21 17:08:38 +0100

    Change the whiteView to redView

--- Baluchon/Controller/WeatherController/WeatherCell.swift
+++ Baluchon/Controller/WeatherController/WeatherCell.swift
@@ -14,7 +14,7 @@ class WeatherCell: UITableViewCell {
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var weatherInformationLabel: UILabel!
-    @IBOutlet weak var whiteView: UIView!
+    @IBOutlet weak var redView: UIView!
     
     // MARK: - NIB
     override func awakeFromNib() {
@@ -26,24 +26,24 @@ class WeatherCell: UITableViewCell {
     // MARK: - PROPERTIES
     var weather: WeatherChannel! {
         didSet {
-//            cityLabel.text = weather.location.city
-//            cityLabel.adjustsFontSizeToFitWidth = true
-//            temperatureLabel.text = "\(weather.item.condition.temp)°C"
-//            weatherInformationLabel.text = weather.item.condition.text
-//            weatherInformationLabel.adjustsFontSizeToFitWidth = true
+            cityLabel.text = weather.location.city
+            cityLabel.adjustsFontSizeToFitWidth = true
+            temperatureLabel.text = "\(weather.item.condition.temp)°C"
+            weatherInformationLabel.text = weather.item.condition.text
+            weatherInformationLabel.adjustsFontSizeToFitWidth = true
         }
     }
     
     // MARK: - METHODS
     private func addShadow() {
-//        whiteView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.7).cgColor
-//        whiteView.layer.shadowRadius = 4.0
-//        whiteView.layer.shadowOffset = CGSize(width: 0, height: 2.0)
-//        whiteView.layer.shadowOpacity = 0.4
+        redView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.7).cgColor
+        redView.layer.shadowRadius = 4.0
+        redView.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+        redView.layer.shadowOpacity = 0.4
     }
     
     private func addCornerRadius() {
-//        whiteView.layer.cornerRadius = 10
+        redView.layer.cornerRadius = 10
     }
     
 }
